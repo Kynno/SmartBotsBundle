91fefb85d97f16096844421940516cdf8eb6f36b
Kynno <9844910+Kynno@users.noreply.github.com>
2019-09-06 12:42:08 +0200

    Add License

--- src/Service/AbstractSmartBotsCommands.php
+++ src/Service/AbstractSmartBotsCommands.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Kynno/SmartBotsBundle package.
+ *
+ * (c) Kynno <contact@kynno.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Kynno\SmartBotsBundle\Service;
 
 use Symfony\Component\HttpClient\HttpClient;
